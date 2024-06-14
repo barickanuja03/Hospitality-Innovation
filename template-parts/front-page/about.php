ae03a0946524d2629f4a09941fa245279d04694e
barickanuja03 <anujabarick001@gmail.com>
2024-06-14 20:10:02 +0530

    about-section

---
+++ template-parts/front-page/about.php
@@ -0,0 +1,54 @@
+<section class="about-section overflow-hidden">
+<div class="row">
+<div class="col-12 col-sm-6 col-md-6 co-lg-6 col-xl-6 px-0 ">   
+ <div class="about-left-child">
+    <DIV CLASS="PARAGRAPH">
+      <h2>
+        ABOUT OUR EVENTS
+      </h2>
+      <p class="first-paragraph">
+        Over USD1 trillion worth of real estate and infrastructure projects, including 315,000 hotel keys, are under development or in the pipeline in the Kingdom of Saudi Arabia. This aggressive construction in Saudi is part of the Kingdoms goal to attract 100 million visitors to its borders as a part of the Vision 2030 plan, making it the leader in hotel clevelopment in the entire of Middle East and Afric
+      </p>
+      <p class="second-paragraph">
+        KSA is followed by United Arab Emirates, a long-standing pioneer in the ion, the UAE Hospitality Market size is expected to grow from USD 1712 milion in 2023 SD 9459.8 million by 2028 with a further 1000 rooms added to the country s extensive 200,000 key portfoli
+      </p>
+      <p class="third-paragraph">
+        With these 2 countries paving the way for futuristic developments and setting standards the world has seldom soon, HIS brings you the best from KSA & UAE.
+      </p>
+    </div>
+ </div>
+</div>
+   <div class="col-12 col-sm-6 col-md-6 co-lg-6 col-xl-6 px-0">
+    <div class="about-right-child">
+     <div class="swiper mySwiper">
+       <div class="swiper-wrapper">
+         <div class="swiper-slide"><img src="https://cdn.suwalls.com/wallpapers/nature/foggy-forest-on-the-river-side-54426-1920x1080.jpg" alt="image"></div>
+         <div class="swiper-slide"><img src="https://img.goodfon.com/wallpaper/nbig/1/4e/nature-view-walk-grass-trees.webp" alt="image"></div>
+         <div class="swiper-slide"><img src="https://cdn.suwalls.com/wallpapers/nature/sunrise-over-the-mountains-17063-1920x1200.jpg"></div>
+         <div class="swiper-slide"><img src="https://tse1.mm.bing.net/th?id=OIP.YAXlTjvtEKchdMVc5laZhwHaE8&pid=Api&P=0&h=180"></div>
+         <div class="swiper-slide"><img src="https://tse3.mm.bing.net/th?id=OIP.zJ3HYpfFdUvLAWDs-OaESAHaFM&pid=Api&P=0&h=180"></div>
+         <div class="swiper-slide"><img src="https://tse4.mm.bing.net/th?id=OIP.IjVtiYa8J6DbPjiCfNdc_AHaF7&pid=Api&P=0&h=180"></div>
+         <div class="swiper-slide"><img src="https://tse4.mm.bing.net/th?id=OIP.ZaSf7GzYb3lmQzzGwXNG4gHaEK&pid=Api&P=0&h=180"></div>
+         <div class="swiper-slide"><img src="https://tse1.mm.bing.net/th?id=OIP.VyeGOidyaY2qOUtlJD55BgHaFE&pid=Api&P=0&h=180"></div>
+         <div class="swiper-slide"><img src="https://tse3.mm.bing.net/th?id=OIP.sYvZzeW9V95lDL4u7QSvWgHaEK&pid=Api&P=0&h=180"></div>
+       </div>
+       <div class="swiper-scrollbar"></div>
+     </div>
+   </div>    
+ </div>
+</div>
+        <script>
+        var swiper = new Swiper(".mySwiper", {
+          scrollbar: {
+            el: ".swiper-scrollbar",
+            hide: false,
+            loop:true,
+          },
+          autoplay: 
+          {
+            delay: 2000,
+          },
+          loop: true,
+        });
+       </script>
+    </section>
